ecdd44cc0b6c3211184474f86d8343acf8f025c7
Lumalizer <7863320+Lumalizer@users.noreply.github.com>
2023-04-26 18:39:20 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from load_data import load_data
 from q1a_plot import q1a_plot
 
 
-options = Options(normalize=False)
+options = Options(normalize=True)
 
 X_train, y_train, X_test, y_test = load_data(options)
 q1a_plot(options, X_train)
