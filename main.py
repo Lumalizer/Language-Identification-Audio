4ebb204b5a2e71b5c817b1c525153303e8f5a5e7
Lumalizer <7863320+Lumalizer@users.noreply.github.com>
2023-04-26 19:07:41 +0200

    prepare model

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from options import Options
 from load_data import load_data
 from q1a_plot import q1a_plot
-
+from model import model
 
 options = Options(normalize=True)
 
 X_train, y_train, X_test, y_test = load_data(options)
-q1a_plot(options, X_train)
+# q1a_plot(options, X_train)
+model = model(options, X_train, y_train, X_test, y_test)
---
+++ model.py
@@ -0,0 +1,6 @@
+from options import Options
+
+
+def model(options: Options, X_train, y_train, X_test, y_test):
+    # TODO
+    pass
