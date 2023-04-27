31c535d58a21bdb26759eba1a9b1dd60e36e5d78
Lumalizer <7863320+Lumalizer@users.noreply.github.com>
2023-04-27 11:26:01 +0200

    rename

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from options import Options
 from load_data import load_data
 from q1a_plot import q1a_plot
-from model import model
+from model import build_model
 
 options = Options(normalize=True)
 
 X_train, y_train, X_test, y_test = load_data(options)
 # q1a_plot(options, X_train)
-model = model(options, X_train, y_train, X_test, y_test)
+build_model = build_model(options, X_train, y_train, X_test, y_test)
--- model.py
+++ model.py
@@ -1,6 +1,6 @@
 from options import Options
 
 
-def model(options: Options, X_train, y_train, X_test, y_test):
+def build_model(options: Options, X_train, y_train, X_test, y_test):
     # TODO
     pass
