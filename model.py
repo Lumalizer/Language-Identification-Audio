8091d74dea8a8e0f2a538ed87018e8eb6282d549
Berk Bayraktaroglu <berkbayraktaroglu@hotmail.com>
2023-04-27 12:37:01 +0200

    Update model.py

--- model.py
+++ model.py
@@ -1,36 +1,38 @@
 from options import Options
 import torch
 import torch.nn as nn
-import torch.nn.functional
+import torch.nn.functional as F
+
 
 class BinaryLanguageClassifier(nn.Module):
-    def __init__(self, options: Options):
+    def __init__(self, options: Options, n_input=1, n_output=1, stride=16, n_channel=32):
         super().__init__()
 
-        # these layers are not correct, but just something as an example
+        # some of these layers might be useful
 
-        # self.conv1 = nn.Conv2d(3, 6, 5)
-        # self.pool = nn.MaxPool2d(2, 2)
-        # self.conv2 = nn.Conv2d(6, 16, 5)
-        # self.fc1 = nn.Linear(16 * 5 * 5, 120)
-        # self.fc2 = nn.Linear(120, 84)
-        # self.fc3 = nn.Linear(84, 10)
+        # self.conv1 = nn.Conv1d(n_input, n_channel, kernel_size=80, stride=stride)
+        # self.bn1 = nn.BatchNorm1d(n_channel)
+        # self.pool1 = nn.MaxPool1d(4)
+        # self.fc1 = nn.Linear(2 * n_channel, n_output)
+
 
     def forward(self, x):
-        # these layers are not correct, but just something as an example
 
-        # x = self.pool(F.relu(self.conv1(x)))
-        # x = self.pool(F.relu(self.conv2(x)))
-        # x = torch.flatten(x, 1)  # flatten all dimensions except batch
-        # x = F.relu(self.fc1(x))
-        # x = F.relu(self.fc2(x))
-        # x = self.fc3(x)
-        return x
+        # won't work yet
+
+        # x = self.conv1(x)
+        # x = F.relu(self.bn1(x))
+        # x = self.pool1(x)
+        # x = F.avg_pool1d(x, x.shape[-1])
+        # x = x.permute(0, 2, 1)
+        # x = self.fc1(x)
+        # return F.log_softmax(x, dim=2)
+
+        return
 
 def build_model(options: Options, X_train, y_train, X_test, y_test):
-
     # train the model here, including defining loss function, optimizer, etc
-
     model = BinaryLanguageClassifier(options)
     
     return model
+
