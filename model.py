391d7d2ac7132da450eb90cee9dfdc68bb9eecb8
m-agata <a.mosinska@tilburguniversity.edu>
2023-05-03 18:48:21 +0200

    Train and test model functions

--- model.py
+++ model.py
@@ -1,6 +1,7 @@
 from options import Options
 import torch
 import torch.nn as nn
+import torch.optim as optim
 import torch.nn.functional as F
 
 
@@ -30,9 +31,44 @@ class BinaryLanguageClassifier(nn.Module):
 
         return
 
-def build_model(options: Options, X_train, y_train, X_test, y_test):
+def train_model(model, device, train_loader):
+    optimizer = optim.Adam(model.parameters(), lr=0.0001) # Another optimiser - Adam
+    scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=20, gamma=0.1)  # reduce the learning after 20 epochs by a factor of 10
+    crossentropy_loss = nn.CrossEntropyLoss() 
+    num_epochs = 3
+    model.train()
+    for epoch in range(num_epochs):
+        for data, labels in train_loader: 
+            data = data.to(device)
+            labels = labels.to(device)
+            optimizer.zero_grad()           
+            predictions = model(data)     
+            loss = crossentropy_loss(predictions, labels) 
+            loss.backward() 
+            optimizer.step()
+    
+    return model
+
+def test_model(model, device, test_loader):
+    model.eval() 
+    test_acc = 0 
+    for data, labels in test_loader: 
+        data = data.to(device)
+        labels = labels.to(device)
+        predictions = model(data)
+        accuracy = (torch.max(predictions, dim=-1, keepdim=True)[1].flatten() == labels).sum() / len(labels)
+        test_acc += accuracy.item()
+    test_acc /= len(test_loader) 
+    return f"Test accuracy {test_acc}"
+
+
+def build_model(options: Options, train_loader, test_loader):
     # train the model here, including defining loss function, optimizer, etc
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     model = BinaryLanguageClassifier(options)
+    train_model(model, device, train_loader)
+    test_model(model, device, test_loader)
+        
     
     return model
 
