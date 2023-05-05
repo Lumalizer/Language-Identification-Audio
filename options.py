5c12d4a38590b9d6bdf6ef71943ab89a6a443f69
Lumalizer <7863320+Lumalizer@users.noreply.github.com>
2023-05-05 11:47:52 +0200

    fix: too low learning rate

--- options.py
+++ options.py
@@ -9,6 +9,6 @@ class Options:
     input_size: int = 40000
     sample_rate: int = 8000
     n_epochs: int = 10
-    lr: float = 0.0001
+    lr: float = 0.001
     device: str = torch.device(
         "cuda:0" if torch.cuda.is_available() else "cpu")
